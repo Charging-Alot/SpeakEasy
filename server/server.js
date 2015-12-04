e25f69008222ae431377ea2136460a8123f72b79
Robert S Crawford <craw.rs@gmail.com>
2015-12-04 10:42:01 -0800

    Committing beginning of the front-end / the server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
-THIS IS ALSO THE BEGINNING
+//THIS IS ALSO THE BEGINNING
+var express = require('express');
+//var middleware = require('./config/middleware.js');
+var http = require('http');
+
+var app = express();
+
+app.use(express.static(__dirname + '/../client'));
+//middleware(app, express);
+
+var port = 8000;
+
+var server = http.createServer(app);
+
+server.listen(port, function () {
+  console.log('listening on port:', port);
+});
+
+module.exports = app;
