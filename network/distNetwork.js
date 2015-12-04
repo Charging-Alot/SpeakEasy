58a69ad6f21850a1590f286923907088ac4d53c4
AVickory <andrew.vickory@gmail.com>
2015-12-04 10:55:08 -0800

    new file distNetwork.

---
+++ network/distNetwork.js
@@ -0,0 +1,8 @@
+var synaptic = require('./synaptic/src/synaptic.js');
+var trainingData = require('./flashyParsing.js');
+
+//i, h, 0 => # input nodes, # hidden nodes, # output nodes
+var MiniNetwork = function (i, h, o) {
+  this.call()
+}
+
