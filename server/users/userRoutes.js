db93f18f3bac878974033e661eefb22c95c9c3d1
Will Dembinski <willdembinski@gmail.com>
2015-12-17 14:33:07 -0800

    Removed the subtitle crawler and fixed the user auth system

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -2,9 +2,6 @@ var userCtrl = require('./userCtrl.js');
 
 
 module.exports = function (app) {
-  // app === userRouter injected from middlware.js
-
-  app.post('/signin', userCtrl.signin);
-  app.post('/signup', userCtrl.signup);
-  app.get('/signedin', userCtrl.checkAuth);
+	app.post('/signin', userCtrl.signin);
+	app.post('/signup', userCtrl.signup);
 };
